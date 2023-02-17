bb9bc5532fa75ba77e893cf3f66427dc1b29dd00
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-18 02:22:25 +0800

    Add Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import { Box, Flex, Link, Button } from '@chakra-ui/react'
+import NextLink from 'next/link'
+
+const Navbar = () => {
+  return (
+    <>This is Navbarar</>
+  )
+}
+
+export default Navbar
