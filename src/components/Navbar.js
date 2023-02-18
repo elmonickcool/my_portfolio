ea3707e36aca7284f310a7d7b62ce4006336ac79
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-18 19:01:04 +0800

    rename to jsx

--- src/components/Navbar.js
+++
@@ -1,8 +0,0 @@
-
-const Navbar = () => {
-  return (
-    <h1 className="text-3xl font-bold underline">This is Navbarar</h1>
-  )
-}
-
-export default Navbar
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+
+const Navbar = () => {
+  return (
+    <h1 className="text-3xl">This is Navbarar</h1>
+  )
+}
+
+export default Navbar
