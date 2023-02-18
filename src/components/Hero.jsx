201df9e88d58f04940656bf924f9c54e076bd3f4
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-18 22:00:54 +0800

    Vertically Centered Hero

---
+++ src/components/Hero.jsx
@@ -0,0 +1,14 @@
+
+const Hero = () => {
+    
+  return (
+    <div className="h-screen flex items-center">
+      <div>
+        <h1 className="font-medium text-4xl">Hello I'm Elmo Nickol</h1>
+        <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,13 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
 
-
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-        {children}
+        <div className='md:container md:mx-auto'>
+           {children}
+        </div>
       <Footer/>
     </>
   )
