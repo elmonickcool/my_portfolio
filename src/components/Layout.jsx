39bdeb16f5cb9f477abac3c6add904b36073ce69
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-18 19:41:25 +0800

    footer routing

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,13 @@
 import Navbar from './Navbar'
+import Footer from './Footer'
 
 
 const Layout = ({ children }) => {
   return (
     <>
       <Navbar />
-      
         {children}
-      
+      <Footer/>
     </>
   )
 }
