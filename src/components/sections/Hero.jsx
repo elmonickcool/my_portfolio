1e962bf8923b2ceabfed1a17c978869b4cf41881
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-20 00:08:35 +0800

    add Github and LinkedIn icons

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,14 +1,21 @@
 import Image from "next/image";
-const Hero = () => {
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+const Hero = () => {
   return (
     <div className="h-screen flex items-center">
-      <div className="mr-8">
-        <h1 className="font-medium text-4xl">Hello, I'm <span className="text-blue-900">Elmo Nickol</span></h1>
+      <div className="ml-4">
+        <h1 className="font-medium text-4xl">
+          Hello, I'm <span className="text-blue-900">Elmo Nickol</span>
+        </h1>
         <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
+        <FontAwesomeIcon icon={faGithub} className="text-3xl text-black" />
+        <FontAwesomeIcon icon={faLinkedin} className="text-3xl text-black" />
       </div>
       <div className="flex justify-end flex-1">
-        <Image src="/hero.png" width={500} height={500}  alt="your-image-alt-text" />
+        <Image src="/hero.png" width={500} height={500} alt="your-image-alt-text" />
       </div>
     </div>
   );
