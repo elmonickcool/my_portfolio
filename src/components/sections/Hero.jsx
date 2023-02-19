d66b14c1ba7b1754c0a76de211e72e4794191faa
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-20 00:58:00 +0800

    nbsp to mx-2

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -11,8 +11,8 @@ const Hero = () => {
           Hello, I'm <span className="text-blue-900">Elmo Nickol</span>
         </h1>
         <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
-        <FontAwesomeIcon icon={faGithub} className="text-3xl text-black" /> &nbsp;
-        <FontAwesomeIcon icon={faLinkedin} className="text-3xl text-black" />
+        <FontAwesomeIcon icon={faGithub} className="text-3xl text-black mx-2" />
+        <FontAwesomeIcon icon={faLinkedin} className="text-3xl text-black mx-2" />
       </div>
       <div className="flex justify-end flex-1">
         <Image src="/hero.png" width={500} height={500} alt="your-image-alt-text" />
