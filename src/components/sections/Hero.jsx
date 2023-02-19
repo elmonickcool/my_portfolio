b0c9b943a5a531f36b03a22e01b6b31fc336b534
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-20 05:07:18 +0800

    import and add NextLink

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "@chakra-ui/react";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import NextLink from "next/link";
 
 const Hero = () => {
   return (
@@ -11,10 +12,10 @@ const Hero = () => {
           Hello, I&apos;m <span className="text-blue-900">Elmo Nickol</span>
         </h1>
         <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
-        <Link href="https://github.com/elmonickcool">
+        <Link as={NextLink} href="https://github.com/elmonickcool">
           <FontAwesomeIcon icon={faGithub} className="text-3xl text-black mx-2" />
         </Link>
-        <Link href="https://www.linkedin.com/in/elmonickcool">
+        <Link as={NextLink} href="https://www.linkedin.com/in/elmonickcool">
           <FontAwesomeIcon icon={faLinkedin} className="text-3xl text-black mx-2" />
         </Link>
       </div>
