09ace3d6302adb4efe16032651ff8ef93ab38f77
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-20 04:53:42 +0800

    added link

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
+import { Link } from "@chakra-ui/react";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 const Hero = () => {
@@ -11,8 +11,12 @@ const Hero = () => {
           Hello, I&apos;m <span className="text-blue-900">Elmo Nickol</span>
         </h1>
         <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
-        <FontAwesomeIcon icon={faGithub} className="text-3xl text-black mx-2" />
-        <FontAwesomeIcon icon={faLinkedin} className="text-3xl text-black mx-2" />
+        <Link href="https://github.com/elmonickcool">
+          <FontAwesomeIcon icon={faGithub} className="text-3xl text-black mx-2" />
+        </Link>
+        <Link href="https://www.linkedin.com/in/elmonickcool">
+          <FontAwesomeIcon icon={faLinkedin} className="text-3xl text-black mx-2" />
+        </Link>
       </div>
       <div className="flex justify-end flex-1">
         <Image src="/hero.png" width={500} height={500} alt="your-image-alt-text" />
