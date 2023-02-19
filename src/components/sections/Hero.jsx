df0a41162ecef5d3ef65e199d5783455ae702fc7
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-20 05:36:13 +0800

    Clean Up

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,8 +1,6 @@
 import Image from "next/image";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Link } from "@chakra-ui/react";
-import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
-import NextLink from "next/link";
+import LinkedinIcon from "../ui/LinkedinIcon";
+import GithubIcon from "../ui/GithubIcon";
 
 const Hero = () => {
   return (
@@ -12,12 +10,8 @@ const Hero = () => {
           Hello, I&apos;m <span className="text-blue-900">Elmo Nickol</span>
         </h1>
         <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
-        <Link as={NextLink} href="https://github.com/elmonickcool">
-          <FontAwesomeIcon icon={faGithub} className="text-3xl text-black mx-2" />
-        </Link>
-        <Link as={NextLink} href="https://www.linkedin.com/in/elmonickcool">
-          <FontAwesomeIcon icon={faLinkedin} className="text-3xl text-black mx-2" />
-        </Link>
+          <GithubIcon/>
+          <LinkedinIcon/>        
       </div>
       <div className="flex justify-end flex-1">
         <Image src="/hero.png" width={500} height={500} alt="your-image-alt-text" />
