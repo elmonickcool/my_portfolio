43d33c4f27cb0c3a353e5e581bad9e632fc6241b
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-19 16:41:17 +0800

    import and positioned img

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,9 +3,12 @@ const Hero = () => {
     
   return (
     <div className="h-screen flex items-center">
-      <div>
+      <div className="mr-8">
         <h1 className="font-medium text-4xl">Hello, I'm <span className="text-blue-900">Elmo Nickol</span></h1>
         <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
+      </div>
+      <div className="flex justify-end flex-1">
+        <img src="hero.png" alt="your-image-alt-text" />
       </div>
     </div>
   );
