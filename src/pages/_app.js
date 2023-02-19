0531fe33b310c56db22eef2dcb18bfbcb7686d62
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-20 00:08:14 +0800

    import fortawesome

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,12 @@ import { ChakraProvider } from '@chakra-ui/react'
 import Layout from '../components/Layout'
 import "../asset/css/global.css"
 import Head from '@/app/head'
+import "@fortawesome/fontawesome-svg-core/styles.css"
+import { config } from "@fortawesome/fontawesome-svg-core";
+// Tell Font Awesome to skip adding the CSS automatically 
+// since it's already imported above
+config.autoAddCss = false;
+
 
 function MyApp({ Component, pageProps }) {
   return (
