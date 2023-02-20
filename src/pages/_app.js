891ef971402117278ee75e45cab9f52598f07ca8
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-20 19:30:11 +0800

    layout darkmode

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,17 +4,21 @@ import Layout from '../components/Layout'
 import "../asset/css/global.css"
 import Head from '@/app/head'
 import "@fortawesome/fontawesome-svg-core/styles.css"
-import { config } from "@fortawesome/fontawesome-svg-core";
+import { config } from "@fortawesome/fontawesome-svg-core"
+import theme from '@/components/ui/theme'
+import { useColorModeValue } from '@chakra-ui/react'
 // Tell Font Awesome to skip adding the CSS automatically 
 // since it's already imported above
 config.autoAddCss = false;
 
 
 function MyApp({ Component, pageProps }) {
+  const bg = useColorModeValue("white","gray.800")
+  const color = useColorModeValue("gray.800","white")
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Head/>
-      <Layout>
+      <Layout bg={bg} color={color}>
         <Component {...pageProps} />
       </Layout>
     </ChakraProvider>
