ce36c723e0a378e1ba422dadb8446af8c304df57
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-21 08:18:03 +0800

    switchable icons in dark mode

--- src/components/ui/DarkModeSwitch.jsx
+++ src/components/ui/DarkModeSwitch.jsx
@@ -1,11 +1,25 @@
-import { useColorMode,Button } from "@chakra-ui/react"
-const DarkModeSwitch = () =>{
-    const {colorMode,toggleColorMode } = useColorMode()
-    return (
-        <Button onClick={toggleColorMode}>
-            Toggle {colorMode === "light" ? "Dark" :"Light"}
-        </Button>
-    )
+import { useColorMode, IconButton } from "@chakra-ui/react"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faMoon, faSun } from "@fortawesome/free-solid-svg-icons"
+
+const DarkModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode()
+
+  return (
+    <IconButton
+      size="md"
+      fontSize="lg"
+      aria-label={`Switch to ${
+        colorMode === "light" ? "dark" : "light"
+      } mode`}
+      variant="ghost"
+      color="current"
+      onClick={toggleColorMode}
+      icon={
+        <FontAwesomeIcon icon={colorMode === "light" ? faMoon : faSun} />
+      }
+    />
+  )
 }
 
-export default DarkModeSwitch;
+export default DarkModeSwitch
