c5ee026e5f85b55393a144b35e4d893cdfce782c
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-21 18:42:59 +0800

    span bold name

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -5,17 +5,17 @@ import DarkModeSwitch from "../ui/DarkModeSwitch";
 
 const Hero = () => {
   return (
-    <div className="h-screen flex items-center">
+    <div className="h-screen items-center flex ">
       <div className="ml-4">
         <h1 className="font-medium text-4xl">
-          Hello, I&apos;m <span className="text-blue-900">Elmo Nickol</span>
+          Hello, I&apos;m <span className="font-bold">Elmo Nickol</span>
         </h1>
         <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
           <GithubIcon/>
           <LinkedinIcon/>
           <DarkModeSwitch/>        
       </div>
-      <div className="flex justify-end flex-1">
+      <div className="flex flex-1 justify-end">
         <Image src="/hero.png" width={500} height={500} alt="your-image-alt-text" />
       </div>
     </div>
