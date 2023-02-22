0a1dc999db21e213a5bcf03ba6b5e6784b44e4fe
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-22 18:56:12 +0800

    import and implement TypeAnimation

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,22 +1,38 @@
-import Image from "next/image";
 import LinkedinIcon from "../ui/LinkedinIcon";
 import GithubIcon from "../ui/GithubIcon";
 import DarkModeSwitch from "../ui/DarkModeSwitch";
+import { TypeAnimation } from "react-type-animation";
 
 const Hero = () => {
   return (
-    <div className="h-screen items-center flex ">
+    <div className="h-screen items-center flex  ">
       <div className="ml-4">
+        <GithubIcon />
+        <LinkedinIcon />
+        <DarkModeSwitch />
         <h1 className="font-medium text-4xl">
           Hello, I&apos;m <span className="font-bold">Elmo Nickol</span>
         </h1>
-        <h3 className="text-2xl">An Aspiring Full Stack Web Developer</h3>
-          <GithubIcon/>
-          <LinkedinIcon/>
-          <DarkModeSwitch/>        
+        <TypeAnimation
+          sequence ={[
+            'An Aspiring Full Stack Web Developer',
+            1000,
+            'An Aspiring Software Engineer',
+            1000,
+            'An Aspiring Front-end Developer',
+            1000,
+            'An Aspiring Back-end Developer',
+            1000,
+          ]}
+          speed={30}
+          style={{ fontSize: '2em' }}
+          wrapper="span"
+          repeat={Infinity}
+        />
+        
       </div>
       <div className="flex flex-1 justify-end">
-        <Image src="/hero.png" width={500} height={500} alt="your-image-alt-text" />
+        <img src="/hero.png" alt="landing image" />
       </div>
     </div>
   );
