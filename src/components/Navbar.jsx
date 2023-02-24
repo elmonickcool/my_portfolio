ca1ab6a4457c1273901c614ba81f198fa526924b
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-25 00:44:11 +0800

    centered route

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,15 @@ import { useState, useEffect } from 'react';
 import NextLink from 'next/link';
 import { Link, useDisclosure } from '@chakra-ui/react';
 import DarkModeSwitch from "../components/ui/DarkModeSwitch";
+import { useRouter } from 'next/router';
+
+
 
 const Navbar = () => {
   const { isOpen, onToggle } = useDisclosure();
   const [isMobile, setIsMobile] = useState(false);
+  const router = useRouter();
+  const currentRoute = router.pathname;
 
   const handleResize = () => {
     setIsMobile(window.innerWidth < 768);
@@ -19,8 +24,8 @@ const Navbar = () => {
 
   return (
     <div className="mb-7">
-      <nav className="flex items-center justify-between flex-wrap bg-gray-800 p-6">
-        <div className="flex items-center flex-shrink-0 text-white mr-6">
+      <nav className="flex items-center justify-between flex-wrap p-6">
+        <div className="flex items-center flex-shrink-0 mr-6">
           <Link as={NextLink} href="/" passHref={true} legacyBehavior={true}>
             <a>
               <span className="font-semibold text-xl tracking-tight">Elmo Nickol</span>
@@ -29,7 +34,7 @@ const Navbar = () => {
         </div>
         <div className="block lg:hidden">
           <button
-            className="flex items-center px-3 py-2 border rounded text-white border-white hover:text-white hover:border-white"
+            className="flex items-center px-3 py-2 border rounded  border-white hover:text-white hover:border-white"
             onClick={onToggle}
           >
             <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
@@ -42,29 +47,29 @@ const Navbar = () => {
           <div className={`text-sm ${isMobile ? 'block' : 'flex'} lg:flex-grow`}>
             <div className={`${isMobile ? '' : 'flex justify-end'}`}>
               <Link as={NextLink} href="/" passHref={true} legacyBehavior={true}>
-                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'} text-white text-xl font-semibold hover:text-white mr-4`}>
+                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'}  text-xl font-semibold mr-4`}>
                   Home
                 </a>
               </Link>
               <Link as={NextLink} href="/project" passHref={true} legacyBehavior={true}>
-                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'} text-white text-xl font-semibold hover:text-white mr-4`}>
+                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'} text-xl font-semibold mr-4`}>
                   Projects
                 </a>
               </Link>
               <Link as={NextLink} href="/about" passHref={true} legacyBehavior={true}>
-                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'} text-white text-xl font-semibold hover:text-white mr-4`}>
+                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'} text-xl font-semibold mr-4`}>
                   About
                 </a>
               </Link>
               <Link as={NextLink} href="/Project" passHref={true} legacyBehavior={true}>
-                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'} text-white text-xl font-semibold hover:text-white mr-4`}>
+                <a className={`block ${isMobile ? 'my-2' : 'mt-4 lg:inline-block lg:mt-0'} text-xl font-semibold mr-4`}>
                   Contact
                 </a>
               </Link>
-              <DarkModeSwitch/>
             </div>
           </div>
         </div>
+        <DarkModeSwitch/>
       </nav>
     </div>
   );
