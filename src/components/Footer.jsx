4c55f92cc7b9cdbc4ebe922fde6cc65e0c58fb0e
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-25 23:59:36 +0800

    draft footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,10 @@
+
 const Footer = () => {
   return (
-    <div>
-      <hr />
-      <h1 className="text-3xl">This is Footer</h1>
+    <div className="container mx-auto">
+      <hr className="mt-5 mb-5 border-b-2 " />
+      <span className="font-semibold text-xl tracking-tight my-5 mb-5">Elmo Nickol</span>
+      {/* FB, IG, TWITTER, EMAIL to the left */}
     </div>
   )
 }
