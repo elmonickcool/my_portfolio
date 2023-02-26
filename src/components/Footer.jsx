cebb0bdc7f03533ac05c65ed64d9a6b1a5cb6f7d
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-02-26 22:07:03 +0800

    format footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,25 @@
+import FacebookIcon from "./ui/FacebookIcon"
+import InstagramIcon from "./ui/InstagramIcon"
+import TwitterIcon from "./ui/TwitterIcon"
 
 const Footer = () => {
   return (
-    <div className="container mx-auto">
-      <hr className="mt-5 mb-5 border-b-2 " />
-      <span className="font-semibold text-xl tracking-tight my-5 mb-5">Elmo Nickol</span>
-      {/* FB, IG, TWITTER, EMAIL to the left */}
+    <div className="container mx-auto my-auto">
+      <div>
+        <hr className="mt-5 mb-5 border-b-2 " />
+      </div>
+      <div className="flex justify-between items-center">
+        <span className="font-semibold text-xl tracking-tight my-5 mb-5">Elmo Nickol</span>
+        {/* FB, IG, TWITTER, EMAIL to the right */}
+        <div>
+          <FacebookIcon />
+          <InstagramIcon />
+          <TwitterIcon />
+
+        </div>
+
+      </div>
+
     </div>
   )
 }
