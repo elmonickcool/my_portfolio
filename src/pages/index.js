6810565cd569128b0063d25d0b369297097fbdd8
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-03 03:54:20 +0800

    import featured

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Hero from "../components/sections/Hero"
 import Stacks from "../components/sections/Stacks"
+import Featured from "@/components/sections/FeaturedProject";
 
 function HomePage() {
     return (
         <div>
             <Hero/>
             <Stacks/>
+            <Featured/>
         </div>
     )
 }
