50742cf019719a0412002f5d1f4035deec27a55b
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-02 18:46:16 +0800

    adjust size

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -43,7 +43,7 @@ const Hero = () => {
         <Spacer />
         <Stack direction={['column', 'column', 'row']} spacing={8} align={['center', 'center', 'flex-start']} mt={[8, 8, 0]}>
           <Box>
-            <Image src="/hero.png" width={600} height={600} alt="landing image"/>
+            <Image src="/hero.png" width={500} height={600} alt="landing image"/>
           </Box>
         </Stack>
       </Flex>
