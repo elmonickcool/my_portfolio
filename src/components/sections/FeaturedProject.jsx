711bbb2e8535a8d7b4f3c422098cc15e20b790e1
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-04 00:29:08 +0800

    Draft Project

--- src/components/sections/FeaturedProject.jsx
+++ src/components/sections/FeaturedProject.jsx
@@ -1,8 +1,18 @@
+import { Heading, Box } from "@chakra-ui/react"
+import Project from "@/pages/projects"
+
 const Featured = () =>{
+    
     return(
-        <div>
-        My Latest Project
-        </div>
+        <>
+        <Box mt={5} mb={5}>
+            <Heading textAlign='center' fontSize='4xl' mb='5'>
+            My Latest Project
+            </Heading>
+        </Box>
+        <Project/>
+        </>
+        
     )
 }
 
