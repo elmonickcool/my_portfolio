b1739e61309b0bf30df8c0ebb6d7da47247e0322
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-03 19:24:15 +0800

    draft overview

---
+++ src/components/sections/Overview.jsx
@@ -0,0 +1,17 @@
+import { Text, Box } from "@chakra-ui/react";
+
+const Overview = () => {
+    return (
+        <Box>
+            <Text fontSize='3xl'>Who am I?</Text>
+            <Text fontSize='xl'>
+                I am a highly motivated software engineer with a passion for innovative
+                software solutions.
+                I have extensive experience developing web applications, APIs, and
+                database-driven software using PHP, Javascript, and MySQL.
+            </Text>
+        </Box>
+    );
+};
+
+export default Overview;
