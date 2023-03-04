5bcde91c198739ccc8a48f161858d119542d442c
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-04 11:02:48 +0800

    config firestore

---
+++ src/pages/contact.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "@firebase/app";
+import { addDoc, collection, getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCUcqxTcbOssPiKqg6Weg256KkUlT_0Vxg",
+  authDomain: "my-contact-form-5be52.firebaseapp.com",
+  projectId: "my-contact-form-5be52",
+  storageBucket: "my-contact-form-5be52.appspot.com",
+  messagingSenderId: "507303115058",
+  appId: "1:507303115058:web:5d9baf3ca19f238e35ff2b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+const Contact =()=>{
+
+
+}
+
+export default Contact
