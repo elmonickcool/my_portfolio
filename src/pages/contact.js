47c900fd103aa442292f762a1cfc93b656f7d738
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-05 18:48:57 +0800

    install uuid and add timestamp, refactor

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,10 @@
 import { initializeApp } from "@firebase/app";
-import { addDoc, collection, getFirestore } from "@firebase/firestore";
-import { FormControl, FormLabel, Input, Button, Box, Heading, useColorMode } from "@chakra-ui/react";
+import { addDoc, collection, getFirestore, serverTimestamp } from "@firebase/firestore";
+import { FormControl, FormLabel, Input, Button, Box, Heading, useColorMode, Textarea } from "@chakra-ui/react";
 import { useState } from "react";
-import { faPhone, faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { faPhone, faEnvelope, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {v4 as uuidv4} from 'uuid';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCUcqxTcbOssPiKqg6Weg256KkUlT_0Vxg",
@@ -22,6 +23,7 @@ const Contact = () => {
     const [email, setEmail] = useState('');
     const [contactnum, setContactNum] = useState('');
     const [message, setMessage] = useState('');
+    const [formSubmitted, setFormSubmitted] = useState(false);
     const { colorMode } = useColorMode();
 
     const handleChange = ({ target: { name, value } }) => {
@@ -51,14 +53,16 @@ const Contact = () => {
     );
 
     const handleSubmit = async (event) => {
+        const id = uuidv4();
         event.preventDefault();
         try {
-            const docRef = await addDoc(collection(db, "contact"), { name, email, contactnum, message });
+            const docRef = await addDoc(collection(db, "contact"), { id,name, email, contactnum, message, timestamp:serverTimestamp() });
             console.log("Document written with ID: ", docRef.id);
             setName('');
             setEmail('');
             setContactNum('');
             setMessage('');
+            setFormSubmitted(true);
         } catch (error) {
             console.error("Error adding document: ", error);
         }
@@ -66,8 +70,15 @@ const Contact = () => {
 
     return (
         <>
-            <Heading textAlign='center'>Contact Me!</Heading>
-            <Box display='flex' justifyContent='space-between' my={8}>
+            
+             {formSubmitted ? (
+                <Box textAlign='center' mt={8}>
+                    <Heading size='lg'>Thank you for filling out the form. I will reply to you via email.</Heading>
+                </Box>
+            ) :(
+                <Heading textAlign='center'>Contact Me!</Heading>
+            )
+}           <Box display='flex' justifyContent='space-between' my={8}>
                 <Box mr={8} mb={4}>
                     Contact Information
                     <Box>
@@ -77,11 +88,15 @@ const Contact = () => {
                         <div mt={2}>
                             {getFontAwesomeIcon(faEnvelope)} elmonickol@gmail.com
                         </div>
+                        <div mt={2}>
+                            {getFontAwesomeIcon(faLocationDot)} Philippines
+                        </div>
                     </Box>
                 </Box>
                 <Box flex={1}>
                     <form onSubmit={handleSubmit}>
-                        <FormControl>
+                        <Box mb={5}>
+                            <FormControl>
                             <FormLabel>Name</FormLabel>
                             <Input type='text' name='name' value={name} onChange={handleChange} />
                         </FormControl>
@@ -95,8 +110,9 @@ const Contact = () => {
                         </FormControl>
                         <FormControl>
                             <FormLabel>Message</FormLabel>
-                            <Input type='textarea' name='message' value={message} onChange={handleChange} />
+                            <Textarea name='message' value={message} onChange={handleChange} />
                         </FormControl>
+                        </Box>
                         <Button type='submit' onClick={handleSubmit}>
                             Submit
                         </Button>
