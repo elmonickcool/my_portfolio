8ded68a5e5a79891e103241d6cac3ce1d250bfbc
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-06 15:21:38 +0800

    added loading screen

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,52 @@
 // pages/_app.js
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, Box } from '@chakra-ui/react'
 import Layout from '../components/Layout'
 import "../asset/css/global.css"
 import Head from '@/app/head'
 import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
 import theme from '@/components/ui/theme'
-import { useColorModeValue } from '@chakra-ui/react'
+import { useColorMode } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+import { PropagateLoader } from 'react-spinners'
+
 // Tell Font Awesome to skip adding the CSS automatically 
 // since it's already imported above
 config.autoAddCss = false;
 
-
 function MyApp({ Component, pageProps }) {
-  const bg = useColorModeValue("white","gray.800")
-  const color = useColorModeValue("gray.800","white")
+  const [loading, setLoading] = useState(true) // <-- move state initialization here
+  useEffect(() => {
+    // Simulate a loading delay
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
+
+  const { colorMode } = useColorMode()
+  const color = colorMode === "light" ? "gray.800" : "white"
+  const spinnerColor = colorMode === "light" ? "gray.800" : "white"
   return (
     <ChakraProvider theme={theme}>
-      <Head/>
-      <Layout bg={bg} color={color}>
-        <Component {...pageProps} />
+      <Head />
+      <Layout bg="white" color={color}>
+        {loading ? (
+          <Box
+            height="100vh"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <PropagateLoader color={spinnerColor} size={20} />
+          </Box>
+        ) : (
+          <Component {...pageProps} />
+        )}
       </Layout>
     </ChakraProvider>
   )
 }
 
 export default MyApp
+
+
