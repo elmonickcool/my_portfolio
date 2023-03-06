503e6023ec87a97e748284dafcd594d47a22c3e8
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-06 18:11:35 +0800

    Add loading state

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,38 @@
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { PropagateLoader } from 'react-spinners'
+import { Box, useColorMode } from '@chakra-ui/react'
+import { useState, useEffect } from 'react'
+
 
 const Layout = ({ children }) => {
+  const [loading, setLoading] = useState(true) 
+  const { colorMode } = useColorMode()
+  const spinnerColor = colorMode === "light" ? "#1A202C" : "white"
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 3000)
+  }, [])
   return (
     <>
       <Navbar />
-        <div className='md:container md:mx-auto'>
+      {loading ? (
+          <Box
+            height="100vh"
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <PropagateLoader color={spinnerColor} size={20} />
+          </Box>
+        ) : (
+          <div className='md:container md:mx-auto'>
            {children}
         </div>
+          )}
+        
       <Footer/>
     </>
   )
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,41 +7,24 @@ import "@fortawesome/fontawesome-svg-core/styles.css"
 import { config } from "@fortawesome/fontawesome-svg-core"
 import theme from '@/components/ui/theme'
 import { useColorMode } from '@chakra-ui/react'
-import { useState, useEffect } from 'react'
-import { PropagateLoader } from 'react-spinners'
+
 
 // Tell Font Awesome to skip adding the CSS automatically 
 // since it's already imported above
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
-  const [loading, setLoading] = useState(true) // <-- move state initialization here
-  useEffect(() => {
-    // Simulate a loading delay
-    setTimeout(() => {
-      setLoading(false)
-    }, 3000)
-  }, [])
+  
 
   const { colorMode } = useColorMode()
   const color = colorMode === "light" ? "gray.800" : "white"
-  const spinnerColor = colorMode === "light" ? "gray.800" : "white"
+  
   return (
     <ChakraProvider theme={theme}>
       <Head />
       <Layout bg="white" color={color}>
-        {loading ? (
-          <Box
-            height="100vh"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <PropagateLoader color={spinnerColor} size={20} />
-          </Box>
-        ) : (
+        
           <Component {...pageProps} />
-        )}
       </Layout>
     </ChakraProvider>
   )
