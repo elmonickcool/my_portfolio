96984feafae794a37bbb2c5c3a0e9f48c9a70a6b
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-22 02:21:14 +0800

    redesign buymeacoffee

--- src/components/ui/BuyMeACoffeeButton.jsx
+++ src/components/ui/BuyMeACoffeeButton.jsx
@@ -2,15 +2,9 @@ import React from 'react';
 
 const BuyMeACoffeeButton = () => {
   return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `
-          <a href="https://www.buymeacoffee.com/elmonickcool" target="_blank" rel="noopener">
-            <img src="https://cdn.buymeacoffee.com/buttons/default-orange.png" alt="Buy Me A Coffee" style="height: 51px !important;width: 217px !important;" >
-          </a>
-        `,
-      }}
-    />
+    <a href="https://www.buymeacoffee.com/elmonickcool" target="_blank">
+      <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height="41" width="174" />
+    </a>
   );
 };
 
