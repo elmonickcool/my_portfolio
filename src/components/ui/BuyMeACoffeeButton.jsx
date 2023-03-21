5b4ece1e6614a0a313d607938e3f5de03dd461b0
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-22 02:52:52 +0800

    optimize Buymeacoffee

--- src/components/ui/BuyMeACoffeeButton.jsx
+++ src/components/ui/BuyMeACoffeeButton.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Image from 'next/image';
 
 const BuyMeACoffeeButton = () => {
   return (
-    <a href="https://www.buymeacoffee.com/elmonickcool" target="_blank">
-      <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height="41" width="174" />
+    <a href="https://www.buymeacoffee.com/elmonickcool" target="_blank" rel="noreferrer">
+      <Image src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height="41" width="174" />
     </a>
   );
 };
