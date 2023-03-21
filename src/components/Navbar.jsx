1aee20629ec29ce37ef68af207b81d81609e8e90
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-22 03:50:56 +0800

    centered navlink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,14 +4,11 @@ import { Link, useDisclosure } from '@chakra-ui/react';
 import DarkModeSwitch from "../components/ui/DarkModeSwitch";
 import { useRouter } from 'next/router';
 
-
-
 const Navbar = () => {
   const { isOpen, onToggle } = useDisclosure();
   const [isMobile, setIsMobile] = useState(false);
   const router = useRouter();
   
-
   const handleResize = () => {
     setIsMobile(window.innerWidth < 768);
   };
@@ -28,48 +25,47 @@ const Navbar = () => {
 
   return (
     <div className="mb-7">
-      <nav className="flex items-center justify-between flex-wrap p-6 ">
-        <div className="flex items-center flex-shrink-0 mr-6">
-          <Link as={NextLink} href="/" passHref={true} legacyBehavior={true}>
-            <a>
-              <span className="font-semibold text-xl tracking-tight">Elmo Nickol</span>
-            </a>
-          </Link>
-        </div>
-        <div className="block lg:hidden">
-          <button
-            className="flex items-center px-3 py-2 border rounded  border-white hover:text-white hover:border-white"
-            onClick={onToggle}
-          >
-            <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-              <title>Menu</title>
-              <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-          </button>
-        </div>
-        <div className={`w-full ${isOpen || !isMobile ? 'block' : 'hidden'} flex items-center w-auto`}>
-          <div className={`text-sm ${isMobile ? 'block' : 'flex'} flex-grow`}>
-            <div className={`${isMobile ? '' : 'flex justify-center'}`}>
-              <Link as={NextLink} href="/projects" passHref={true} legacyBehavior={true}>
-                <a className={`block text-xl  mr-4 ${isActive('/projects') ? 'font-bold' : 'font-medium'}`}>
-                  Projects
-                </a>
-              </Link>
-              <Link as={NextLink} href="/about" passHref={true} legacyBehavior={true}>
-                <a className={`block text-xl  mr-4 ${isActive('/about') ? 'font-bold' : 'font-medium'}`}>
-                  About
-                </a>
-              </Link>
-              <Link as={NextLink} href="/contact" passHref={true} legacyBehavior={true}>
-                <a className={`block text-xl mr-4 ${isActive('/contact') ? 'font-bold' : 'font-medium'}`}>
-                  Contact
-                </a>
-              </Link>
-            </div>
-          </div>
-          <DarkModeSwitch/>
-        </div>
-      </nav>
+      <div className="flex items-center justify-between flex-wrap p-6">
+  <div className="flex items-center flex-shrink-0 mr-6 flex-grow">
+    <Link as={NextLink} href="/" passHref={true} legacyBehavior={true}>
+      <a>
+        <span className="font-semibold text-xl tracking-tight">Elmo Nickol</span>
+      </a>
+    </Link>
+  </div>
+  <div className="block lg:hidden">
+    <button
+      className="flex items-center px-3 py-2 border rounded  border-white hover:text-white hover:border-white"
+      onClick={onToggle}
+    >
+      <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+        <title>Menu</title>
+        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+      </svg>
+    </button>
+  </div>
+  <div className={`w-full ${isOpen || !isMobile ? 'block' : 'hidden'} flex items-center w-auto flex-grow`}>
+    <div className={`text-sm ${isMobile ? 'block' : 'flex'} justify-center`}>
+      <Link as={NextLink} href="/projects" passHref={true} legacyBehavior={true}>
+        <a className={`block text-xl  mr-4 ${isActive('/projects') ? 'font-bold' : 'font-medium'}`}>
+          Projects
+        </a>
+      </Link>
+      <Link as={NextLink} href="/about" passHref={true} legacyBehavior={true}>
+        <a className={`block text-xl  mr-4 ${isActive('/about') ? 'font-bold' : 'font-medium'}`}>
+          About
+        </a>
+      </Link>
+      <Link as={NextLink} href="/contact" passHref={true} legacyBehavior={true}>
+        <a className={`block text-xl mr-4 ${isActive('/contact') ? 'font-bold' : 'font-medium'}`}>
+          Contact
+        </a>
+      </Link>
+    </div>
+    
+  </div>
+  <DarkModeSwitch className="flex-grow"/>
+</div>
     </div>
   );
 };
