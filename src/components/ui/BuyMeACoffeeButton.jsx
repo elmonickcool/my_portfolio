768e8e4cf43367dafee90c694e3b2de0d60d50b8
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-22 03:18:15 +0800

    fix buymeacoffee

--- src/components/ui/BuyMeACoffeeButton.jsx
+++ src/components/ui/BuyMeACoffeeButton.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import Image from 'next/image';
+import { Image } from "@chakra-ui/react";
+
 
 const BuyMeACoffeeButton = () => {
   return (
     <a href="https://www.buymeacoffee.com/elmonickcool" target="_blank" rel="noreferrer">
-      <Image src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height="41" width="174" />
+      <Image src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" height="50" width="174" />
     </a>
   );
 };
