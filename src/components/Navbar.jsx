a808889cc5f50427c49a08117244ac40399b350f
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-03-22 12:30:48 +0800

    test active

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -50,23 +50,19 @@ const Navbar = () => {
         <div className={`w-full ${isOpen || !isMobile ? 'block' : 'hidden'} lg:flex lg:items-center lg:w-auto`}>
           <div className={`text-sm ${isMobile ? 'block' : 'flex'} lg:flex-grow`}>
             <div className={`${isMobile ? '' : 'flex justify-end'}`}>
-              <Link as={NextLink} href="/" passHref={true} legacyBehavior={true}>
-                <a className={`block text-xl font-semibold mr-4 ${isActive('/') ? 'font-bold underline ' : 'font-medium'}`}>
-                  Home
-                </a>
-              </Link>
+              
               <Link as={NextLink} href="/projects" passHref={true} legacyBehavior={true}>
-                <a className={`block text-xl font-semibold mr-4 ${isActive('/projects') ? 'font-bold underline' : 'font-medium'}`}>
+                <a className={`block text-xl mr-4 ${isActive('/projects') ? 'font-bold ' : 'font-medium'}`}>
                   Projects
                 </a>
               </Link>
               <Link as={NextLink} href="/about" passHref={true} legacyBehavior={true}>
-                <a className={`block text-xl font-semibold mr-4 ${isActive('/about') ? 'font-bold underline' : 'font-medium'}`}>
+                <a className={`block text-xl  mr-4 ${isActive('/about') ? 'font-bold ' : 'font-medium'}`}>
                   About
                 </a>
               </Link>
               <Link as={NextLink} href="/contact" passHref={true} legacyBehavior={true}>
-                <a className={`block text-xl font-semibold mr-4 ${isActive('/contact') ? 'font-bold underline' : 'font-medium'}`}>
+                <a className={`block text-xl  mr-4 ${isActive('/contact') ? 'font-bold' : 'font-medium'}`}>
                   Contact
                 </a>
               </Link>
