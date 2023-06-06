3433b78b61b5fb7b32a9ec061e05bd6bd44ae553
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-06-07 03:22:22 +0800

    shorten about me paragraph

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,4 +1,4 @@
-import { Stack, Text, Image, Box, Button,HStack } from "@chakra-ui/react";
+import { Stack, Text, Image, Box, Button, HStack } from "@chakra-ui/react";
 import { useColorMode } from "@chakra-ui/color-mode";
 import BuyMeACoffeeButton from "@/components/ui/BuyMeACoffeeButton";
 import Stacks from "../components/sections/Stacks"
@@ -8,38 +8,36 @@ const About = () => {
 
   return (
     <>
-    <Stack direction={{ base: "column", md: "row" }} spacing={8} alignItems={{ md: "center" }} justifyContent={{ base: "center", md: "flex-start" }}  me={3} className="h-full">
-      <Image src="/elmonickol.jpg" width="30%" height="30%" ms={4} me={5} alt="about image" borderRadius="lg" boxShadow="lg" />
-      <Box ms={5} textAlign={{ base: "center", md: "left" }}>
-        <Text fontSize="3xl" fontWeight="bold" textAlign="center" mb={5}>
-          About Me
-        </Text>
-        <Text fontSize={{ base: "xl", md: "2xl" }} mt={4} mb={{ base: 3, md: 0 }}>
-          Hello and welcome to my portfolio website! My name is Elmo Nickol Laplap, and I am a 26-year-old Full Stack Web Developer from Quezon, Bukidnon. I graduated from Xavier University - Ateneo de Cagayan with a BS in Information Systems in 2019, and since then, I have pursued my passion for web development.
-        </Text>
-        <Text fontSize={{ base: "xl", md: "2xl" }} mt={4} mb={{ base: 3, md: 0 }}>
-          I have extensive experience in programming languages such as PHP, JavaScript, Python, and Java, and I am always striving to learn more and stay up-to-date with the latest developments in the field. I am committed to developing innovative and high-quality web applications that provide an optimal user experience.
-        </Text>
-        <Text fontSize={{ base: "xl", md: "2xl" }} mt={4} mb={{ base: 3, md: 0 }}>
-          In pursuit of my dream job as a Full Stack Web Developer, I attended the Kodego bootcamp to expand my skill set and knowledge. The bootcamp was a valuable learning experience that helped me sharpen my coding abilities and gain practical experience working on real-world projects.
-        </Text>
-        <HStack mt={8}>
-          <Button
-          as="a"
-          href="/Resume.pdf"
-          download="ElmoNickolLaplap_Resume.pdf"
-          colorScheme={colorMode === "light" ? "blue" : "teal"}
-          size="lg"
-        >
-          Download Resume
-        </Button>
-        <BuyMeACoffeeButton />
-        </HStack>
-      </Box>
-    </Stack>
-    <Stacks/>
+      <Stack direction={{ base: "column", md: "row" }} spacing={8} alignItems={{ md: "center" }} justifyContent={{ base: "center", md: "flex-start" }} me={3} className="h-full">
+        <Image src="/elmonickol.jpg" width="30%" height="30%" ms={4} me={5} alt="about image" borderRadius="lg" boxShadow="lg" />
+        <Box ms={5} textAlign={{ base: "center", md: "left" }}>
+          <Text fontSize="3xl" fontWeight="bold" textAlign="center" mb={5}>
+            About Me
+          </Text>
+          <Text fontSize={{ base: "xl", md: "2xl" }} mt={4} mb={{ base: 3, md: 0 }}>
+            Welcome to my portfolio website! I'm Elmo Nickol Laplap, a 26-year-old Full Stack Web Developer from Quezon, Bukidnon. 
+            I graduated from Xavier University - Ateneo de Cagayan in 2019 with a BS in Information Systems. 
+            With expertise in PHP, JavaScript, Python, and Java, I continuously learn and stay updated in the field. 
+            My goal is to create innovative web applications that offer exceptional user experiences. 
+            I attended the Kodego bootcamp to enhance my skills and gain practical experience on real-world projects.
+          </Text>
+          <HStack mt={8}>
+            <Button
+              as="a"
+              href="/Resume.pdf"
+              download="ElmoNickolLaplap_Resume.pdf"
+              colorScheme={colorMode === "light" ? "blue" : "teal"}
+              size="lg"
+            >
+              Download Resume
+            </Button>
+            <BuyMeACoffeeButton />
+          </HStack>
+        </Box>
+      </Stack>
+      <Stacks />
     </>
-    
+
   );
 };
 
