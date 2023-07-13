0a25f14bc1bef1697f36a01a2dce6e31bb55e29f
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-07-13 21:40:13 +0800

    required fields and redesign

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -20,113 +20,123 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [contactnum, setContactNum] = useState('');
-    const [message, setMessage] = useState('');
-    const [formSubmitted, setFormSubmitted] = useState(false);
-    const { colorMode } = useColorMode();
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contactnum, setContactNum] = useState('');
+  const [message, setMessage] = useState('');
+  const [formSubmitted, setFormSubmitted] = useState(false);
+  const { colorMode } = useColorMode();
 
-    const handleChange = ({ target: { name, value } }) => {
-        switch (name) {
-            case 'name':
-                setName(value);
-                break;
-            case 'email':
-                setEmail(value);
-                break;
-            case 'contactnum':
-                setContactNum(value);
-                break;
-            case 'message':
-                setMessage(value);
-                break;
-            default:
-                break;
-        }
-    };
+  const handleChange = ({ target: { name, value } }) => {
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'email':
+        setEmail(value);
+        break;
+      case 'contactnum':
+        setContactNum(value);
+        break;
+      case 'message':
+        setMessage(value);
+        break;
+      default:
+        break;
+    }
+  };
 
-    const getFontAwesomeIcon = (icon) => (
-        <FontAwesomeIcon
-            icon={icon}
-            className={`text-xl mx-2 ${colorMode === "light" ? "text-black" : "text-white"}`}
-        />
-    );
+  const getFontAwesomeIcon = (icon) => (
+    <FontAwesomeIcon
+      icon={icon}
+      className={`text-xl mx-2 ${colorMode === "light" ? "text-black" : "text-white"}`}
+    />
+  );
 
-    const handleSubmit = async (event) => {
-        const id = uuidv4();
-        event.preventDefault();
-        try {
-            const docRef = await addDoc(collection(db, "contact"), { id, name, email, contactnum, message, timestamp: serverTimestamp() });
-            console.log("Document written with ID: ", docRef.id);
-            setName('');
-            setEmail('');
-            setContactNum('');
-            setMessage('');
-            setFormSubmitted(true);
-        } catch (error) {
-            console.error("Error adding document: ", error);
-        }
-    };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
 
-    return (
-        <>
-            <Flex flexDirection={["column", "row"]} justifyContent={["center","space-around"]} alignItems={["center", "flex-start"]}>
-                <Box w={["100%","50%"]} textAlign={["center", "left"]} bg='white' p={5} borderRadius={3}> 
-                    {formSubmitted ? (
-                        <Heading size='lg' mb={8}>Thank you for filling out the form. I will reply to you via email.</Heading>
-                    ) : (
-                        <Heading textAlign='center' mb={8}>Get in touch</Heading>
-                    )
-                    }
-                    <form onSubmit={handleSubmit}>
-                        <Box mb={5} ms={3} me={3}>
-                            <FormControl>
-                                <FormLabel>Name</FormLabel>
-                                <Input type='text' name='name' value={name} onChange={handleChange} />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Email</FormLabel>
-                                <Input type='email' name='email' value={email} onChange={handleChange} />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Contact</FormLabel>
-                                <Input type='text' name='contactnum' value={contactnum} onChange={handleChange} />
-                            </FormControl>
-                            <FormControl>
-                                <FormLabel>Message</FormLabel>
-                                <Textarea name='message' value={message} onChange={handleChange} />
-                            </FormControl>
-                        </Box>
-                        <Button type='submit' onClick={handleSubmit} mt={5} w={["50%","100%"]}>
-                            Send Message
-                        </Button>
-                    </form>
-                </Box>
-                <Box border="1px" borderRadius={5} p={5} ml={[0,5]} mt={[10,0]} bg='white'>
-                    <Heading mb={5} >Contact Information</Heading>
-                    <Box me={5} textAlign={["center", "left"]}>
-                        <Text mt={5}>
-                            {getFontAwesomeIcon(faPhone)} +639164728907
-                        </Text>
-                        <Link href='mailto:elmonickol@gmail.com'>
-                            <Text mt={5}>
-                                {getFontAwesomeIcon(faEnvelope)} elmonickol@gmail.com
-                            </Text>
-                        </Link>
-                        <Link href="https://goo.gl/maps/XAoQcfPffVfVD83c8">
-                            <Text mt={5}>
-                                {getFontAwesomeIcon(faLocationDot)} Balangay 2, Poblacion, Quezon, Bukidnon
-                            </Text>
-                        </Link>
-                    </Box>
-                    
-                </Box>
-                
-            </Flex>
-           
-        </>
-    );
+    // Check if any required field is empty
+    if (!name || !email || !contactnum || !message) {
+      alert("Please fill out all the required fields.");
+      return;
+    }
+
+    const id = uuidv4();
+
+    try {
+      const docRef = await addDoc(collection(db, "contact"), {
+        id,
+        name,
+        email,
+        contactnum,
+        message,
+        timestamp: serverTimestamp()
+      });
+
+      console.log("Document written with ID: ", docRef.id);
+      setName('');
+      setEmail('');
+      setContactNum('');
+      setMessage('');
+      setFormSubmitted(true);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+  };
+
+  return (
+    <>
+      <Flex flexDirection={["column", "row"]} justifyContent={["center", "space-around"]} alignItems={["center", "flex-start"]}>
+        <Box w={["100%", "50%"]} textAlign={["center", "left"]} p={5} borderRadius={3}>
+          <Box mb={5} ms={3} me={3} border="1px" p={5} borderRadius={2}>
+            {formSubmitted ? (
+              <Heading size="lg" mb={8}>Thank you for filling out the form. I will reply to you via email.</Heading>
+            ) : (
+              <form onSubmit={handleSubmit}>
+                <Heading textAlign="center" mb={8}>Get in touch</Heading>
+                <FormControl>
+                  <FormLabel>Name</FormLabel>
+                  <Input type="text" border="1px" name="name" value={name} onChange={handleChange} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" border="1px" name="email" value={email} onChange={handleChange} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Contact Number</FormLabel>
+                  <Input type="number" pattern="[0-9]*" inputMode="numeric" border="1px" name="contactnum" value={contactnum} onChange={handleChange} />
+                </FormControl>
+                <FormControl>
+                  <FormLabel>Message</FormLabel>
+                  <Textarea name="message" border="1px" value={message} onChange={handleChange} />
+                </FormControl>
+                <Button type="submit" onClick={handleSubmit} mt={5} w={["50%", "100%"]}>Send Message</Button>
+              </form>
+            )}
+          </Box>
+        </Box>
+        <Box border="1px" borderRadius={5} p={5} ml={[0, 5]} mt={[10, 0]}>
+          <Heading mb={5} >Contact Information</Heading>
+          <Box me={5} textAlign={["center", "left"]}>
+            <Text mt={5}>
+              {getFontAwesomeIcon(faPhone)} +639164728907
+            </Text>
+            <Link href='mailto:elmonickol@gmail.com'>
+              <Text mt={5}>
+                {getFontAwesomeIcon(faEnvelope)} elmonickol@gmail.com
+              </Text>
+            </Link>
+            <Link href="https://goo.gl/maps/XAoQcfPffVfVD83c8">
+              <Text mt={5}>
+                {getFontAwesomeIcon(faLocationDot)} Balangay 2, Poblacion, Quezon, Bukidnon
+              </Text>
+            </Link>
+          </Box>
+        </Box>
+      </Flex>
+    </>
+  );
 }
 
 export default Contact;
