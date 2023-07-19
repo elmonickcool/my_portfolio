1e3c8a8dd6d54109e88e6c9931ab9b70ff5bfae8
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-07-19 12:27:38 +0800

    remove box

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -30,101 +30,102 @@ const Project = () => {
   };
 
   // Determine the number of columns based on screen size
-  const gridColumns = useBreakpointValue({ base: 1, sm: 2, md: 2, lg:4});
+  const gridColumns = useBreakpointValue({ base: 1, sm: 2, md: 2, lg: 4 });
 
   return (
     <>
       <Heading textAlign="center" fontSize="4xl" mb="5">
         My Project
       </Heading>
-      <Box display="flex" justifyContent="center" alignItems="center">
-        <SimpleGrid columns={gridColumns} spacing={5}>
-          {projectShowcase.length > 0 ? (
-            projectShowcase.map((project) => (
-              <article key={project.title}>
-                <Box
-                  maxW="sm"
-                  bg={bgColor[colorMode]}
-                  borderRadius="5%"
-                  pt={2.5}
-                  boxShadow="md"
-                  transition="transform 0.2s"
-                  _hover={{ transform: 'scale(1.05)' }}
-                >
-                  <Box m={3} >
-                    <Image src={project.img} borderRadius="2.5%" alt="Project" />
+      <SimpleGrid columns={gridColumns} spacing={5} justifyContent="center" alignItems="center">
+        {projectShowcase.length > 0 ? (
+          projectShowcase.map((project) => (
+            <article
+              key={project.title}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                maxW="sm"
+                bg={bgColor[colorMode]}
+                borderRadius="5%"
+                pt={2.5}
+                boxShadow="md"
+                transition="transform 0.2s"
+                _hover={{ transform: 'scale(1.05)' }}
+              >
+                <Box m={3}>
+                  <Image src={project.img} borderRadius="2.5%" alt="Project" />
+                </Box>
+                <Box p={6}>
+                  <Heading size="md">{project.title}</Heading>
+                  <Box mt={4}>
+                    <p>{project.description}</p>
                   </Box>
-                  <Box p={6}>
-                    <Heading size="md">{project.title}</Heading>
-                    <Box mt={4}>
-                      <p>{project.description}</p>
-                    </Box>
-                    <Box mt={4}>
-                      {Array.isArray(project.stack) ? (
-                        project.stack.map((stack, index) => (
-                          <Badge key={index} m="1">
-                            {stack.trim()}
-                          </Badge>
-                        ))
-                      ) : (
-                        <Badge>{project.stack}</Badge>
-                      )}
-                    </Box>
-                    <Box mt={4}>
-                      <Flex justifyContent="space-between">
-                        <Button
-                          as={Link}
-                          href={project.live}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          variant="outline"
-                          leftIcon={
-                            <FontAwesomeIcon
-                              icon={faGlobe}
-                              className={`text-3xl ${
-                                colorMode === 'light'
-                                  ? 'text-black'
-                                  : 'text-white'
-                              }`}
-                            />
-                          }
-                          onClick={() => handleLinkClick(project.live)}
-                        >
-                          Live Demo
-                        </Button>
-                        <Button
-                          as={Link}
-                          href={project.github}
-                          target="_blank"
-                          rel="noopener noreferrer"
-                          variant="outline"
-                          leftIcon={
-                            <FontAwesomeIcon
-                              icon={faGithub}
-                              className={`text-3xl ${
-                                colorMode === 'light'
-                                  ? 'text-black'
-                                  : 'text-white'
-                              }`}
-                            />
-                          }
-                          onClick={() => handleLinkClick(project.github)}
-                        >
-                          Repository
-                        </Button>
-                      </Flex>
-                    </Box>
+                  <Box mt={4}>
+                    {Array.isArray(project.stack) ? (
+                      project.stack.map((stack, index) => (
+                        <Badge key={index} m="1">
+                          {stack.trim()}
+                        </Badge>
+                      ))
+                    ) : (
+                      <Badge>{project.stack}</Badge>
+                    )}
+                  </Box>
+                  <Box mt={4}>
+                    <Flex justifyContent="space-between">
+                      <Button
+                        as={Link}
+                        href={project.live}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        variant="outline"
+                        leftIcon={
+                          <FontAwesomeIcon
+                            icon={faGlobe}
+                            className={`text-3xl ${
+                              colorMode === 'light' ? 'text-black' : 'text-white'
+                            }`}
+                          />
+                        }
+                        onClick={() => handleLinkClick(project.live)}
+                      >
+                        Live Demo
+                      </Button>
+                      <Button
+                        as={Link}
+                        href={project.github}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        variant="outline"
+                        leftIcon={
+                          <FontAwesomeIcon
+                            icon={faGithub}
+                            className={`text-3xl ${
+                              colorMode === 'light' ? 'text-black' : 'text-white'
+                            }`}
+                          />
+                        }
+                        onClick={() => handleLinkClick(project.github)}
+                      >
+                        Repository
+                      </Button>
+                    </Flex>
                   </Box>
                 </Box>
-              </article>
-            ))
-          ) : (
-            <Box textAlign="center" fontSize="xl">
-              No project showcases found.
-            </Box>
-          )}
-        </SimpleGrid>
-      </Box>
+              </Box>
+            </article>
+          ))
+        ) : (
+          <Box textAlign="center" fontSize="xl">
+            No project showcases found.
+          </Box>
+        )}
+      </SimpleGrid>
     </>
   );
 };
