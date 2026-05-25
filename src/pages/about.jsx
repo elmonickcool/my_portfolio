3116ddf9391419f740f0a58c0ef77c28886112c7
elmonickcool <elmonickol@gmail.com>
2026-05-26 06:49:19 +0800

    redesign about page

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -5,56 +5,62 @@ import {
   Box,
   Button,
   HStack,
-  useBreakpointValue,
+  useColorMode,
   Heading,
   VStack,
   Divider,
 } from "@chakra-ui/react"
-import { useColorMode } from "@chakra-ui/react"
+
 import BuyMeACoffeeButton from "@/components/ui/BuyMeACoffeeButton"
 import Stacks from "../components/sections/Stacks"
 
 const About = () => {
   const { colorMode } = useColorMode()
 
-  const showImage = useBreakpointValue({ base: false, md: true })
-
   return (
-    <Box px={{ base: 5, md: 16 }} py={10}>
+    <Box px={{ base: 5, md: 16 }} py={{ base: 10, md: 16 }}>
       <Stack
         direction={{ base: "column", md: "row" }}
         spacing={{ base: 10, md: 16 }}
         align="center"
       >
         {/* IMAGE */}
-        {showImage && (
-          <Box flex="1" display="flex" justifyContent="center">
-            <Image
-              src="/elmonickol.jpg"
-              alt="About Me"
-              borderRadius="2xl"
-              boxShadow="2xl"
-              objectFit="cover"
-              w={{ base: "200px", md: "300px" }}
-              h={{ base: "200px", md: "300px" }}
-            />
-          </Box>
-        )}
+        <Box
+          flex="1"
+          display="flex"
+          justifyContent="center"
+        >
+          <Image
+            src="/elmonickol.jpg"
+            alt="About Me"
+            borderRadius="2xl"
+            boxShadow="2xl"
+            objectFit="cover"
+            w={{ base: "180px", sm: "220px", md: "300px" }}
+            h={{ base: "180px", sm: "220px", md: "300px" }}
+          />
+        </Box>
 
         {/* TEXT */}
-        <Box flex="2">
-          <VStack align={{ base: "center", md: "start" }} spacing={4}>
+        <Box flex="2" w="100%">
+          <VStack
+            align={{ base: "center", md: "start" }}
+            spacing={4}
+            textAlign={{ base: "center", md: "left" }}
+            maxW="600px"
+            mx={{ base: "auto", md: "0" }}
+          >
             <Heading fontSize={{ base: "2xl", md: "4xl" }}>
               About Me
             </Heading>
 
-            <Text fontSize={{ base: "md", md: "lg" }} color="gray.500" textAlign={{ base: "center", md: "left" }}>
+            <Text fontSize={{ base: "md", md: "lg" }} color="gray.500">
               Developer focused on building clean, fast, and user-friendly web applications.
             </Text>
 
             <Divider />
 
-            <VStack align={{ base: "center", md: "start" }} spacing={2}>
+            <VStack align="start" spacing={2} w="100%">
               <Text><b>Name:</b> Elmo Nickol Laplap</Text>
               <Text><b>Age:</b> 29</Text>
               <Text><b>Location:</b> Quezon, Bukidnon</Text>
@@ -68,7 +74,12 @@ const About = () => {
             </VStack>
 
             {/* BUTTONS */}
-            <HStack pt={4} spacing={4}>
+            <HStack
+              pt={4}
+              spacing={4}
+              flexWrap="wrap"
+              justify={{ base: "center", md: "flex-start" }}
+            >
               <Button
                 as="a"
                 href="/Resume.pdf"
@@ -86,7 +97,7 @@ const About = () => {
       </Stack>
 
       {/* SKILLS SECTION */}
-      <Box mt={16}>
+      <Box mt={{ base: 12, md: 16 }}>
         <Stacks />
       </Box>
     </Box>
